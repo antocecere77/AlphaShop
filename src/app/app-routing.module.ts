72fbeaed23ad6535b517ab9829e09f0f179f7212
Antonio Cecere <antocecere77@gmail.com>
2019-07-08 07:35:36 +0200

    Aggiunta route articoli con tabella articoli (ngFor)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { ErrorComponent } from './error/error.component';
+import { ArticoliComponent } from './articoli/articoli.component';
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'welcome/:userid', component: WelcomeComponent},
+  {path: 'articoli', component: ArticoliComponent},
   {path: '**', component: ErrorComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { ArticoliComponent } from './articoli/articoli.component';
 
 
 @NgModule({
@@ -14,7 +15,8 @@ import { ErrorComponent } from './error/error.component';
     AppComponent,
     WelcomeComponent,
     LoginComponent,
-    ErrorComponent
+    ErrorComponent,
+    ArticoliComponent
   ],
   imports: [
     BrowserModule,
