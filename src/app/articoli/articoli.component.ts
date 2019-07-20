53a57109268b38e671027e7cba611803a1979da3
Antonio Cecere <antocecere77@gmail.com>
2019-07-20 18:00:00 +0200

    Aggiunto filtro di ricerca per descrizione articolo

--- src/app/articoli/articoli.component.ts
+++ src/app/articoli/articoli.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ArticoliDataService } from '../services/data/articoli-data.service';
 
 export class Articoli {
 
   constructor(
-    public codArt: string,
+    public codart: string,
     public descrizione: string,
     public um: string,
-    public pzCart: number,
-    public pesoNetto: number,
+    public pzcart: number,
+    public peso: number,
     public prezzo: number,
-    public idStatoArt: boolean,
-    public dataCreaz: Date) {
+    public isactive: boolean,
+    public data: Date
 
-  }
+  ) { }
+
 }
 
 @Component({
@@ -24,30 +26,53 @@ export class Articoli {
 export class ArticoliComponent implements OnInit {
 
   NumArt = 0;
+
   pagina = 1;
   righe = 10;
+
+  filter: string;
   articoli: Articoli[];
 
   /*
   articoli = [
-    new Articoli('014600301', 'BARILLA FARINA 1 KG', 'PZ', 24, 1, 1.09, true, new Date()),
-    new Articoli('013500121', 'BARILLA PASTA GR.500 N.70 1/2 PENNE', 'PZ', 30, 0.5, 1.3, true, new Date()),
-    new Articoli('007686402', 'FINDUS FIOR DI NASELLO 300 GR', 'PZ', 8, 0.3, 6.46, true, new Date()),
-    new Articoli('057549001', 'FINDUS CROCCOLE 400 GR', 'PZ', 12, 0.4, 5.97, true, new Date())];
+    new Articoli('014600301','BARILLA FARINA 1 KG','PZ',24,1,1.09,true,new Date()),
+    new Articoli('013500121','BARILLA PASTA GR.500 N.70 1/2 PENNE','PZ',30,0.5,1.3,true,new Date()),
+    new Articoli('007686402','FINDUS FIOR DI NASELLO 300 GR','PZ',8,0.3,6.46,true,new Date()),
+    new Articoli('057549001','FINDUS CROCCOLE 400 GR','PZ',12,0.4,5.97,true,new Date())
+  ]
   */
 
-  constructor(private articoliService: ArticoliDataService) { }
+  constructor(private route: ActivatedRoute, private articoliService: ArticoliDataService) { }
 
   ngOnInit() {
 
-    this.articoliService.getArticoli('Barilla').subscribe(
-      response => {
+    this.filter = this.route.snapshot.params.filter;
 
-        console.log(response);
-        this.articoli = response;
-        this.NumArt = this.articoli.length;
-      }
-    );
+    if (this.filter !== undefined) {
+      this.getArticoli(this.filter);
+    }
+
   }
 
+  refresh() {
+    this.getArticoli(this.filter);
+  }
+
+  public getArticoli(filter: string) {
+
+    this.articoliService.getArticoli(filter).subscribe(
+      response => {
+
+        console.log('Ricerchiamo articoli con filtro ' + filter);
+
+        this.articoli = response;
+        console.log(this.articoli);
+
+        this.NumArt = this.articoli.length;
+        console.log(this.articoli.length);
+      }
+
+    );
+
+  }
 }
